44c46f1c9b094b713f9476ea39d1d958188f800b
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2024-05-12 14:44:31 +0600

    some minor css changes

--- src/components/PdfShow.jsx
+++ src/components/PdfShow.jsx
@@ -2,16 +2,14 @@ import PropTypes from "prop-types";
 const PdfShow = ({ rowData }) => {
   if (!rowData) {
     return (
-      <div className="m-3 bg-slate-100  border-2 border-gray-400 rounded-sm">
-        <div className="flex">
-          <div>
-            <img src="/public/Frc.png" alt="frc" />
-          </div>
-          <div className="  text-[60px]  font-palanquin text-gray-600 pb-6 font-bold ml-72">
+      <div className="m-3 bg-slate-100  border-2 border-gray-400 rounded-sm ">
+        <div className="flex max-lg:flex-col items-center justify-self-center">
+          <img src="/public/Frc.png" alt="frc" />
+          <div className=" text-[60px]  font-palanquin text-gray-600 pb-6 font-bold lg:ml-72 ">
             <p>DVST Report</p>
           </div>
         </div>
-        <div className="flex flex-col m-4 p-2 text-gray-700 text-md border-2 border-dashed border-gray-400  border-spacing-7 min-h-36"></div>
+        <div className="flex flex-col m-2 p-2 text-gray-700 text-md border-2 border-dashed border-gray-400  border-spacing-7 font-palanquin gap-2 min-h-36"></div>
       </div>
     );
   }
