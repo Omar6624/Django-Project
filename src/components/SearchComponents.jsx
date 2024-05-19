b8860c9b513ebba72160d77cc0d0682ad70c4063
Omar6624 <86346592+Omar6624@users.noreply.github.com>
2024-05-19 11:07:34 +0600

    search implimented

--- src/components/SearchComponents.jsx
+++ src/components/SearchComponents.jsx
@@ -1,29 +1,58 @@
 // import { Fragment } from "react";
-import { useState } from "react";
+import PropTypes from "prop-types";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import TextField from "@mui/material/TextField";
-import dayjs from "dayjs";
-export const SearchComponents = () => {
-  const [value, setValue] = useState(dayjs());
+export const SearchComponents = ({
+  dateValue,
+  searchValue,
+  handleText,
+  handleBlur,
+  handleDate,
+  handleBlurDate,
+}) => {
+  const handleClear = () => {
+    handleBlur();
+    handleBlurDate();
+  };
   return (
-    <div className="container p-4 mx-auto flex justify-between">
-      <TextField
-        id="search"
-        label="Search"
-        variant="standard"
-        color="success"
-        sx={{ width: 220 }}
-      />
-
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          label="Controlled picker"
-          value={value}
-          onChange={(newValue) => setValue(newValue)}
+    <div className="flex justify-between container p-4 mx-auto items-center ">
+      <div className=" flex gap-7">
+        <TextField
+          id="search"
+          label="Search"
+          variant="standard"
+          color="success"
+          sx={{ width: 220 }}
+          value={searchValue}
+          onChange={handleText}
         />
-      </LocalizationProvider>
+
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            label="Date Search"
+            onChange={(newValue) => handleDate(newValue)}
+            value={dateValue}
+          />
+        </LocalizationProvider>
+      </div>
+      <div>
+        <button
+          className="custom-btn delay-50 text-center items-center py-2 px-5"
+          onClick={handleClear}
+        >
+          Clear
+        </button>
+      </div>
     </div>
   );
+};
+SearchComponents.propTypes = {
+  dateValue: PropTypes.object.isRequired,
+  searchValue: PropTypes.string.isRequired,
+  handleText: PropTypes.func.isRequired,
+  handleBlur: PropTypes.func.isRequired,
+  handleDate: PropTypes.func.isRequired,
+  handleBlurDate: PropTypes.func.isRequired,
 };
